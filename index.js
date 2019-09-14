d2722eebb62e4f241a15e7627a91ff56f4d35ec3
luisaga215 <luis.aga215@gmail.com>
2019-09-14 17:17:19 -0500

    Init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + 'scripts'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+})
+
+var server = app.listen(8080, function() {
+  console.log("Server in port 8080");
+})
