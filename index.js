00b24037e713934ddd59805d669dbe610220b595
luisaga215 <luis.aga215@gmail.com>
2019-09-15 01:05:46 -0500

    top row

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + 'scripts'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
